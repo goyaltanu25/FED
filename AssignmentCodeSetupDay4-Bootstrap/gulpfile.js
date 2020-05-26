a8770078445bd4a81c9a251fbf2f210302147761
tanu.b.goyal <tanu.b.goyal@accenture.com>
2020-05-26 23:24:26 +0530

    FED first commit

---
+++ AssignmentCodeSetupDay4-Bootstrap/gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+
+const browserSync = require('browser-sync').create();
+
+
+//compile scss into css
+function style() {
+    return gulp.src('src/scss/main.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+}
+
+
+
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./src",
+           index: "/index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style)
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
+ 
+
+
+ 
